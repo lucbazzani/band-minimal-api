c4d9ca9d40fdccb651f23b804ab562029570c411
Lucas Bazzani <lu.bazzani@hotmail.com>
2026-04-15 20:49:56 -0300

    Add Swagger UI and Create Route Class.

---
+++ Routes/BandRoute.cs
@@ -0,0 +1,11 @@
+﻿namespace Band.Routes;
+
+public static class BandRoute
+{
+    // key word "this" as a parameter means it is an extension method
+    public static void BandRoutes(this WebApplication app)
+    {
+        app.MapGet("band", () => "Olá, Banda!");
+    }
+}
+
